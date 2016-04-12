00ea5948cc503d9b7daa917181fe2441c2ec8c1e
yeekevin11 <yeekevin11@gmail.com>
2016-04-12 16:14:05 -0400

    Added support for gift-to-tree navigation. Untested and bugs likely

--- src/navigation/finalNavigation.py
+++ src/navigation/finalNavigation.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python
 
-import numpy as np
-import time
-import cv2
 import mapping
+#from Adafruit_ADS1x15 import ADS1x15
 
 def moveRobotForward():
     # Move robot forward one grid space
@@ -24,11 +22,43 @@ def turnRobotAround():
 
 def update_intersection(sensorL, sensorM, sensorR):
     # Check surroundings for available paths
-    path_lst = [False]*3
-    return path_lst
+
+    gain = 4096
+    sps = 250
+
+    voltsL = sensorL.readADCSingleEnded(0,gain,sps)/1000
+    distanceL = irDistFunction(voltsL)
+    
+    voltsM = sensorM.readADCSingleEnded(1,gain,sps)/1000
+    distanceM = irDistFunction(voltsM)
+    
+    voltsR = sensorR.readADCSingleEnded(2,gain,sps)/1000
+    distanceR = irDistFunction(voltsR)
+
+    path_lst=[False]*3
+
+    path_lst[0] = distanceL > 5
+    path_lst[1] = distanceM > 5
+    path_lst[2] = distanceR > 5
+    
+    return path_lst 
+
+def irDistFunction(volts):
+    # Function to calculate distance from sensor input
+    ''' TODO: Calibrate for specific sensor '''
+    return 12.374 * volts**(-1.09)
+
+def pixyDetectItem():
+    # Insert pixy code here
+    # return 'gift' if gift is detected, 'tree' if tree is detected, else ''
+    return ''
 
 def pickUpGift():
     # Pick up the gift
+    pass
+
+def dropGift():
+    # Drop the gift at the tree
     pass
 
 # Initialize first node
@@ -43,11 +73,14 @@ path_lst=[False,True,False]
 # Initialize that tree and gift have not been found
 giftFound = False
 treeFound = False
+giftDropped = False
 
-''' TODO: Initialize sensors '''
-sensorL = 0
-sensorM = 0
-sensorR = 0
+# Initialize sensors
+sensorL=ADS1x15(ic=ADS1015)
+sensorM=ADS1x15(ic=ADS1015)
+sensorR=ADS1x15(ic=ADS1015)
+
+''' TODO: Initialize Pixy '''
 
 # Create second node based on initialized tree_lst and path_lst
 mapping.node_proc(map_dic, tree_lst, path_lst)
@@ -69,31 +102,96 @@ while True:
         if path_lst[1] or path_lst[2]:
             mapping.node_proc(map_dic, tree_lst, path_lst)
 
+        # Mark opposite turn if tree is found
+        if treeFound:
+            if tree_lst[2]:
+                turnList[turnIndex] = 'R'
+                turnIndex += 1
+            else:
+                turnIndex -= 1
+                del turnList[turnIndex]
+
     # Check if straight is available
     elif path_lst[1]:
-        # Keep robot straight here
+        # Keep robot straight
 
         if path_lst[2]:
             mapping.node_proc(map_dic, tree_lst, path_lst)
+
+        # Mark middle if tree is found
+        if treeFound:
+            if tree_lst[2] == 'f':
+                turnList[turnIndex] = 'M'
+                turnIndex += 1
+            else:
+                turnIndex -= 1
+                del turnList[turnIndex]
 
     # Check if right is available
     elif path_lst[2]:
         turnRobotRight()
 
+        # Mark opposite turn if tree is found
+        if treeFound:
+            if tree_lst[2]:
+                turnList[turnIndex] = 'L'
+                turnIndex += 1
+            else:
+                turnIndex -= 1
+                del turnList[turnIndex]
+
     # Deadend has been reached
     else:
 
-        # Insert Pixy code here
-        if giftFound:
+        # Check if there's an item
+        itemFound = pixyDetectItem()
+
+        if itemFound == 'gift':
+            giftFound = True
             pickUpGift()
 
             if treeFound: 
-                # Navigate back to tree
-                pass
+                # Exit and navigate back to tree
+                turnRobotAround()
+                break
 
-        elif treeFound:
-            # Begin tracking turns to get back to tree
-            pass
+        elif itemFound == 'tree':
+            treeFound = True
+
+            if giftFound:
+                dropGift()
+                giftDropped = True
+                break
+            
+            turnList = {1: 'G'}
+            turnIndex = 2
+
+        # Swap current node and previous node
+        temp = tree_lst[1]
+        tree_lst[1] = tree_lst[0]
+        tree_lst[0] = temp
+
+        # Change direction
+        tree_lst[2] = 'b'
+
+        turnRobotAround()
+
+
+while not giftDropped:
+
+    moveRobotForward()
+
+    # Access previous turn
+    turnIndex -= 1
+    currTurn = turnList[turnIndex]
+
+    # Turn depending on last command. Drop gift
+    if currTurn == 'L':
+        turnRobotLeft()
+    elif currTurn == 'R':
+        turnRobotRight()
+    elif currTurn == 'G':
+        dropGift()
 
 
 
